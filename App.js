eda4a8385b6222cd18d402be9f5fb76d137610e2
Nourshaolin <N.ElMoujahid@aui.ma>
2024-02-17 00:56:32 +0100

    update 0

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open dtyrtydrt fgefgfddto start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5DC',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
